5c7ca24b2c9ede2b712025c896c49ac521ebf0fc
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-07-29 13:13:33 +0000

    Add navbar to index page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello App</h1>
+      <NavBar/>
     </div>
   );
 }
