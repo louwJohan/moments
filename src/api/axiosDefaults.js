16577adf1a8979a9b61b9c62f1972613874ae9ac
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-08-03 13:31:44 +0000

    Add logout functionality

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://moments-ci-ljc.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/from-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
