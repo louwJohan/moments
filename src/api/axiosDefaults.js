cd0bc9808a80946c864ac0aaa83eeb6e2ada94fc
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-08-15 13:02:26 +0000

    final deployment

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://moments-ci-ljc.herokuapp.com/";
+axios.defaults.baseURL = "https://moments-ci-ljc.herokuapp.com";
 axios.defaults.headers.post["Content-Type"] = "multipart/from-data";
 axios.defaults.withCredentials = true;
 
